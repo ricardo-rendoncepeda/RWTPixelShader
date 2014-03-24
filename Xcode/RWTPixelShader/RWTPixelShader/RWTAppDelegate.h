d3af9133bb02eaba62eed6c443e9f27285b686a7
Ricardo Rendon Cepeda <ricardo.rendon.cepeda@idean.com>
2014-03-23 17:29:32 -0700

    Configured project

--- Xcode/RWTPixelShader/RWTPixelShader/RWTAppDelegate.h
+++ Xcode/RWTPixelShader/RWTPixelShader/RWTAppDelegate.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 RayWenderlich. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface RWTAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
